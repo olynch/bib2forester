d1d3e58ab382068ad2d4e443301ab8c52666c314
Owen Lynch <root@owenlynch.org>
2024-12-30 23:28:28 +0000

    added ability to pull from doi

--- bib2forester.py
+++ bib2forester.py
@@ -7,6 +7,36 @@ import argparse
 import io
 from pathlib import Path
 from unicodedata import category
+import requests
+
+## This code is stolen from pubs
+
+def _get_request(url, headers=None):
+    """GET requests to a url. Return the `requests` object.
+
+    :raise ConnectionError:  if anything goes bad (connection refused, timeout
+                             http status error (401, 404, etc)).
+    """
+    try:
+        r = requests.get(url, headers=headers)
+        r.raise_for_status()
+        return r
+    except requests.exceptions.RequestException as e:
+        raise ReferenceNotFoundError(e.args)
+
+
+    ## DOI support
+
+def doi2bibtex(doi, **kwargs):
+    """Return a bibtex string from a DOI"""
+
+    url = 'https://dx.doi.org/{}'.format(doi)
+    headers = {'accept': 'application/x-bibtex'}
+    r = _get_request(url, headers=headers)
+    if r.encoding is None:
+        r.encoding = 'utf8'  # Do not rely on guessing from request
+
+    return r.text
 
 def get_args():
     parser = argparse.ArgumentParser(
@@ -14,7 +44,8 @@ def get_args():
                         description='converts bib files to forester',
                         epilog='')
 
-    parser.add_argument('bibfile')
+    parser.add_argument('-b', '--bib')
+    parser.add_argument('-D', '--doi')
     parser.add_argument('destdir')
 
     return parser.parse_args()
@@ -57,6 +88,8 @@ def tree(e):
     buf.write("\\taxon{reference}\n")
     if 'doi' in e.fields_dict:
         buf.write("\\meta{{doi}}{{{}}}\n".format(e.fields_dict['doi'].value))
+    elif 'DOI' in e.fields_dict:
+        buf.write("\\meta{{doi}}{{{}}}\n".format(e.fields_dict['DOI'].value))
     elif 'url' in e.fields_dict:
         buf.write("\\meta{{external}}{{{}}}\n".format(e.fields_dict['url'].value))
     name = "{}-{}-{}".format(author_part(e), year_part(e), title_part(e))
@@ -71,8 +104,13 @@ def tree(e):
 
 def main():
     args = get_args()
-    bibtex_database = bibtexparser.parse_file(
-        args.bibfile,
+    if args.doi:
+        bibs = doi2bibtex(args.doi)
+    elif args.bib:
+        with open(args.bib, 'r') as f:
+            bibs = f.read()
+    bibtex_database = bibtexparser.parse_string(
+        bibs,
         append_middleware=[SeparateCoAuthors(), SplitNameParts()]
     )
     for e in bibtex_database.entries:
