8380497d635ddb9fe6548eb18ece8d98c090a01c
Owen Lynch <root@owenlynch.org>
2024-12-30 23:40:06 +0000

    removed misleading nix shell line

--- bib2forester.py
+++ bib2forester.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env nix-shell
-#! nix-shell -i python3 -p python3 python3Packages.bibtexparser
-
 import bibtexparser
 from bibtexparser.middlewares import MergeNameParts, MergeCoAuthors, SeparateCoAuthors, SplitNameParts
 import argparse
